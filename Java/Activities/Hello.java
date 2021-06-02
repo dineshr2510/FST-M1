1980c32aaed83a27fb6dcfb139943b112f904f21
dineshr2510 <84365818+dineshr2510@users.noreply.github.com>
2021-06-02 23:49:32 +0530

    Add files via upload

---
+++ Java/Activities/Hello.java
@@ -0,0 +1,34 @@
+
+public class Hello {
+
+			public static void main(String[] args) {
+			
+				String name = "Abhiram";
+				System.out.println(name);
+				
+				int myNum = 15;
+				System.out.println(myNum);
+				System.out.println("Hello, World!");
+				String firstName = "John";
+				String lastName = "Doe";
+				System.out.println(firstName + " " + lastName); // Outputs "John Doe"
+				
+				int myNum1 = 15;
+					
+				myNum1 = 20; // myNum is now 20
+					
+				System.out.println(myNum1);
+				int myNum2 = 150;           // Integer (whole number)
+				@SuppressWarnings("unused")
+				float myFloatNum = 5.99f; // Floating point number
+				@SuppressWarnings("unused")
+				char myLetter = 'D';      // Character
+				@SuppressWarnings("unused")
+				boolean myBool = true;    // Boolean
+				@SuppressWarnings("unused")
+				String myText = "Hello";  // String
+				System.out.println(myNum2);
+		}
+		
+}
+
